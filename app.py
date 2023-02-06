256ff2fce9b75858c1e5ccd6020a037b29abeb75
Antonin <antonin.faure@epfl.ch>
2023-02-06 15:59:17 +0100

    regex fix

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ def room():
     if (name == None):
         return redirect(url_for('home')) 
 
-    name = re.sub(r"([A-Za-z0-9\-\.])", "", name)
-
+    name = re.sub(r"(?![A-Za-z0-9\-\.])", "", name)
+ 
     room = db.rooms.find_one({'name': name})
     if (room == None):
         return redirect(url_for('home'))
