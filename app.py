e338c23751dc1794fd1d96976e9148e62e0ba954
Antonin <antonin.faure@epfl.ch>
2023-02-18 20:42:06 +0100

    course page, semester schedule

--- app.py
+++ app.py
@@ -108,6 +108,127 @@ def find_free_rooms():
 
     return rooms_name
 
+@app.route('/find_semester', methods=['GET'])
+def find_semester():
+    section = request.args.get('section')
+    promo = request.args.get('semester')
+    courses_ids = list(map(lambda x: x['course'], list(db.plans.find({ 'section': section, 'promo': promo }))))
+
+    courses = list(db.courses.find({ '_id' : { '$in' : courses_ids} }))
+
+    if (len(courses) == 0):
+        return redirect(url_for('home'))
+
+    bookings = list(db.booking.find({ 'course': {'$in' : list(map(lambda x: x['_id'], courses))}}))
+
+    for schedule in bookings:
+        course = db.courses.find_one({ '_id': schedule['course'] })
+        schedule['course'] = course
+        room = db.rooms.find_one({ '_id': schedule['room'] })
+        schedule['room'] = room
+
+    days = ['Lu', 'Ma', 'Me', 'Je', 'Ve']
+    times = range(8, 20)
+    timetable = dict()
+    colspan = {'Lu' : 1, 'Ma' : 1, 'Me' : 1, 'Je' : 1, 'Ve' : 1}
+
+    for i, time in enumerate(times):
+        timetable[f'{time}-{time+1}'] = dict()
+        for j, day in enumerate(days):
+            timetable[f'{time}-{time+1}'][day] = []
+            for schedule in bookings:
+                if (schedule['day'] == day and schedule['time'] == f'{time}-{time+1}'):
+                    solved = False
+                    # List all slots at that time-day
+                    for i_slot, slot in enumerate(timetable[f'{time}-{time+1}'][day]):
+                        # If the same course then add room to list
+                        if ('course' in slot and schedule['course']['code'] == slot['course']['code']):
+                            timetable[f'{time}-{time+1}'][day][i_slot]['rooms'].append(schedule['room']['name'])
+                            solved = True
+
+                    # If conflict not solved then append new slot
+                    if (solved == False):
+                        timetable[f'{time}-{time+1}'][day].append({
+                                'course' : {
+                                    'code': schedule['course']['code'],
+                                    'name': schedule['course']['name']
+                                },
+                                'time': schedule['time'],
+                                'day': schedule['day'],
+                                'label': schedule['label'],
+                                'duration': schedule['duration'],
+                                'rooms': [schedule['room']['name']],
+                                'colspan' : 1
+                            })
+
+            for k in range(i):
+                k_time = f'{times[k]}-{times[k]+1}'
+                if (len(timetable[k_time][day]) > 0):
+                    for slot in timetable[k_time][day]:
+                        if ('duration' in slot and slot['duration'] > i - k):
+                            timetable[f'{time}-{time+1}'][day].append({'skip': True})
+
+    # Find max colspan per day
+    for time in times:
+        for day in days:
+            cols = len(timetable[f'{time}-{time+1}'][day])
+            if (cols > colspan[day]):
+                colspan[day] = cols
+    
+    # Fill colspan
+    for time in times:
+        for day in days:
+            cols = len(timetable[f'{time}-{time+1}'][day])
+            if (day == 'Lu'):
+                print(f'{time}-{time+1} / {day} = {cols}')
+            if (cols > 0 and cols < colspan[day]):
+                max_cols = cols
+                if ('skip' not in timetable[f'{time}-{time+1}'][day][0]):
+                    for k in range(timetable[f'{time}-{time+1}'][day][0]['duration']):
+                        if (len(timetable[f'{k+time}-{k+time+1}'][day]) > max_cols):
+                            max_cols = len(timetable[f'{k+time}-{k+time+1}'][day])
+                timetable[f'{time}-{time+1}'][day][0]['colspan'] = colspan[day] - max_cols + 1
+
+    return render_template('semester.html', colspan=colspan, timetable=timetable)
+
+@app.route('/course', methods=['GET'])
+def find_course():
+    code = request.args.get('code')
+    course = list(db.courses.find({ 'code' : code}))[0]
+    bookings = list(db.booking.find({ 'course' : course['_id']}))
+    for i, schedule in enumerate(bookings):
+        room = db.rooms.find_one({ '_id' : schedule['room']})
+        bookings[i]['room'] = room
+
+    days = ['Lu', 'Ma', 'Me', 'Je', 'Ve']
+    times = range(8, 20)
+    timetable = dict()
+
+    for i, time in enumerate(times):
+        timetable[f'{time}-{time+1}'] = dict()
+        for j, day in enumerate(days):
+            timetable[f'{time}-{time+1}'][day] = dict()
+            for schedule in bookings:
+                if (schedule['day'] == day and schedule['time'] == f'{time}-{time+1}'):
+                    if ('rooms' in timetable[f'{time}-{time+1}'][day]):
+                        timetable[f'{time}-{time+1}'][day]['rooms'].append(schedule['room']['name'])
+                    else:
+                        timetable[f'{time}-{time+1}'][day] = {
+                                'time': schedule['time'],
+                                'day': schedule['day'],
+                                'label': schedule['label'],
+                                'duration': schedule['duration'],
+                                'rooms': [schedule['room']['name']],
+                            }
+
+            for k in range(i):
+                k_time = f'{times[k]}-{times[k]+1}'
+                if (len(timetable[k_time][day]) > 0):
+                    if ('duration' in timetable[k_time][day] and timetable[k_time][day]['duration'] > i - k):
+                        timetable[f'{time}-{time+1}'][day] = {'skip': True}
+    return render_template('course.html', course=course, timetable=timetable)
+    
+
 @app.errorhandler(404)
 def page_not_found(e):
     return redirect(url_for('home'))
